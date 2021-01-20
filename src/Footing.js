c6a2371665dc97df13bbebd9f8aa1fbee4bf2b8f
dfeeney96 <daniellefeeney96@gmail.com>
2021-01-20 17:19:52 +0000

    React installed for app

---
+++ src/Footing.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Footing.css';
+
+export default function Footing (){
+    return(
+<div className="Footing">
+
+<span> <a href="/" target="_blank" rel="noreferrer" > Open Source Code by D Feeney </a> </span>
+<span><a href= "https://www.linkedin.com/in/danielle-feeney-2050011a5/" target="_blank" rel="noreferrer" > View Linked In</a></span>
+
+</div>
+
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Footing from './Footing';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footing />
   </React.StrictMode>,
   document.getElementById('root')
 );
